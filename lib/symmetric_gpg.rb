d5f594b4fb8ca014d298daa24ddbe8b82df25e4f
CarlosJHR64 <carlosjhr64@gmail.com>
2011-06-23 06:09:58 -0700

    version bug

--- lib/symmetric_gpg.rb
+++ lib/symmetric_gpg.rb
@@ -2,7 +2,7 @@ require 'open3'
 
 # A gpg command line wrapper for symmetric encryption
 module SymmetricGPG
-  VERSION = '1.4.0'
+  VERSION = '2.0.1'
 
   CRYPTOR = 'gpg -q --batch --passphrase-fd 0'
   ENCRYPTING = '--force-mdc --symmetric'
